611aa275893b5e8fcb4aaa37d0c9c9aab25cc81a
Jake Clarke <jake.t.clarke@gmail.com>
2021-12-22 23:56:54 +1100

    update linting

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-# libraries
-import json
-import os
-import sys
-
+"""
+CMD line utilty to do the weighting
+"""
 import pandas as pd
-from guts.functions import *
-from guts.weighter import ModeSelect, Weight, RunOptions, SelectInputFile
-from pyfiglet import Figlet
-from clint.arguments import Args
-from clint.textui import puts, indent, colored
 import click
-from PyInquirer import Token, ValidationError, Validator, print_json, prompt, style_from_dict
-from guts.validators import EmptyValidator, FilePathValidator
-
+from guts.functions import log
+from guts.weighter import ModeSelect, Weight, RunOptions, SelectInputFile
 
 @click.command()
 def main():
+    """CMD line utilty to do the weighting
+    """
     log("Geographic Weighter", color="green", figlet=True)
     log("Welcome to the Geographic Weighter", "green")
-    log("This tool will take an input file with properties for a geography, such as census data by postcode, and output it by a different geography, for example federal electorates", "green")
-    log("This process calculates population weighted percentages for the input fields, so requires a numeric input for each property, as well as a total population column for the area", "green")
+    log("This tool takes an input file with properties for an area, eg  data by SA1,", "green")
+    log("It then outputs it by a different area, eg by federal electorate", "green")
+    log("This process calculates population weighted percentages for the input fields,", "green")
+    log("so requires a numeric input for each property,", "green")
+    log("as well as a total population column for the area", "green")
     log("To get started, enter the path to your file below:", "green")
 
     file = SelectInputFile()
@@ -31,7 +28,7 @@ def main():
     data = pd.read_csv(file.input_file)
 
     # initialise a weight object with the geog type specified
-    w = Weight(input_mode=mode.input_mode,
+    weight = Weight(input_mode=mode.input_mode,
                output_mode=mode.output_mode,
                input_file=file.input_file)
 
@@ -41,12 +38,12 @@ def main():
     options.prompt()
 
     # update weight class with data entered
-    w.update_properties(options.answers)
+    weight.update_properties(options.answers)
 
     # implement postcode / state electorate
     # replace below with a call to the class
     # try:
-    w.run()
+    weight.run()
     # except:
     #     log(f"Unfortunately, something went wrong", color="red")
     #     log(f"{w}", color='green')
