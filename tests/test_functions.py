63c257bc3f5d5553b7b178d95aae7dcca227bcf9
Jake Clarke <jake.t.clarke@gmail.com>
2021-12-23 10:40:49 +1100

    fix linting

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,46 +1,64 @@
-from guts.weighter import *
+
+"""
+Tests for the functions in guts.functions
+"""
+import os
+from pathlib import Path
 from guts.functions import get_filename_from_path
 from guts.functions import get_filename_from_path_without_extension
 from guts.functions import empty_directory
 from guts.functions import make_directorytree_if_not_exists
-import pytest
-import os
-from pathlib import Path
 
 def test_get_filename_from_path_nofilepath():
+    """
+    Test for getting filename from a filepath - no path
+    """
 
-    s = 'test.csv'
-    assert get_filename_from_path(s) == s
+    filepath = 'test.csv'
+    assert get_filename_from_path(filepath) == 'test.csv'
 
 def test_get_filename_from_path_works():
-    
-    s = 'dir1/test.csv'
-    assert get_filename_from_path(s) == 'test.csv'
+    """
+    Test for getting filename from a filepath - handle a directory
+    """
+
+    filepath = 'dir1/test.csv'
+    assert get_filename_from_path(filepath) == 'test.csv'
 
 def test_get_filename_from_path_nofilepath_two():
-    
-    s = 'test.xlsx'
-    assert get_filename_from_path(s) == s
+    """
+    Test for getting filename from a filepath - no path
+    """
+    filepath = 'test.xlsx'
+    assert get_filename_from_path(filepath) == 'test.xlsx'
 
 def test_get_filename_from_path_manypaths():
-    
-    s = 'dir1/dir2/dir3/test.csv'
-    assert get_filename_from_path(s) == 'test.csv'
+    """
+    Test for getting filename from a filepath - multiple dirs
+    """
+    filepath = 'dir1/dir2/dir3/test.csv'
+    assert get_filename_from_path(filepath) == 'test.csv'
 
 def test_get_filename_from_path_without_extension_works():
-    
-    s = 'dir1/test.csv'
-    assert get_filename_from_path_without_extension(s) == 'test'
+    """
+    Test for getting filename without extension from a filepath - no path, no extension
+    """
+    filepath = 'dir1/test'
+    assert get_filename_from_path_without_extension(filepath) == 'test'
 
 def test_get_filename_from_path_without_extension_nofilepath_two():
-    
-    s = 'test.xlsx'
-    assert get_filename_from_path_without_extension(s) == 'test'
+    """
+    Test for getting filename without extension from a filepath - no path
+    """
+    filepath = 'test.xlsx'
+    assert get_filename_from_path_without_extension(filepath) == 'test'
 
 def test_get_filename_from_path_without_extension_manypaths():
-    
-    s = 'dir1/dir2/dir3/test.csv'
-    assert get_filename_from_path_without_extension(s) == 'test'
+    """
+    Test for getting filename without extension from a filepath - no path
+    """
+    filepath = 'dir1/dir2/dir3/test.csv'
+    assert get_filename_from_path_without_extension(filepath) == 'test'
 
 def test_empty_directory_works():
     """
