be42e6902f22547be6dd7d6a417cb8a1e9bd66ae
Jake Clarke <jake.t.clarke@gmail.com>
2021-12-23 10:50:00 +1100

    format with black

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ Fixture set for tests
 import pytest
 from guts.functions import empty_directory
 
+
 @pytest.fixture()
 def ensure_clean_output():
     """
     Deletes files from default output directory to ensure tests function properly
     """
-    empty_directory('output')
+    empty_directory("output")
     return True
