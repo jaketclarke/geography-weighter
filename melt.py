93a3224345c6ca9572c573a7b332b212a1fa5f28
Jake Clarke <jake.t.clarke@gmail.com>
2021-12-23 17:56:00 +1100

    add ranking function

--- melt.py
+++ melt.py
@@ -8,4 +8,12 @@ df.to_csv('output/2016Census_G01_VIC_SED_2022_unpivot_sorted.csv')
 
 out = df.sort_values(by=['census_variable', 'value'])
 
+# out.to_csv('output/2016Census_G01_VIC_SED_2022_unpivot_sorted.csv')
+
+tmp = out.groupby('census_variable').size()
+rank = tmp.map(range)
+rank =[item for sublist in rank for item in sublist]
+out['rank'] = rank
+out["rank"] = out["rank"] + 1
+
 out.to_csv('output/2016Census_G01_VIC_SED_2022_unpivot_sorted.csv')
