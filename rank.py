14ff50df3c6cd45ba44bdffa3602ab9f775b3213
Jake Clarke <jake.t.clarke@gmail.com>
2022-07-01 14:17:05 +1000

    update rank code to match bulk rank

--- rank.py
+++ rank.py
@@ -17,6 +17,12 @@ rank =[item for sublist in rank for item in sublist]
 out['rank'] = rank
 out["rank"] = out["rank"] + 1
 
+# if all the input data is NaN, don't rank
+input_data.loc[input_data['value'].isnull(), 'rank'] = np.nan
+
+output_file = input_file.replace('unpivoted','ranked_unpivoted')
+input_data.to_csv(f'output{os.sep}{output_file}', index=False, na_rep='Null')
+
 out.to_csv('output/2016Census_G01_VIC_SED_2022_unpivot_sorted_processed.csv', index="False", na_rep='Null')
 
 
